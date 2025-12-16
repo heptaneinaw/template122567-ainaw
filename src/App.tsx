f93a330ebced739d22cd7fa867d132f86badc6ac
Dev Heptane <dev@heptane.de>
2025-12-16 19:58:16 +0100

    Fix base path and React Router basename for GitHub Pages

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/template122567-ainaw">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/contact" element={<Contact />} />
